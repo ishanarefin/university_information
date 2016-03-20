c96488a4aa729469187beb5b0564b90012721945
ishanarefin <ishanarefin@gmail.com>
2016-03-20 22:33:32 +0600

    Make database, prepared full register activitivy, done with insertion, created needed activities

--- app/src/main/java/com/wordpress/ishansaysjava/bangladeshiuniversityinfo/LoginActivity.java
+++ app/src/main/java/com/wordpress/ishansaysjava/bangladeshiuniversityinfo/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.wordpress.ishansaysjava.bangladeshiuniversityinfo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,25 +15,10 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void sendToRegister(View view)
+    {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+        finish();;
     }
 }
--- app/src/main/java/com/wordpress/ishansaysjava/bangladeshiuniversityinfo/UserUniversityDatabase.java
+++ app/src/main/java/com/wordpress/ishansaysjava/bangladeshiuniversityinfo/UserUniversityDatabase.java
@@ -1,7 +1,36 @@
 package com.wordpress.ishansaysjava.bangladeshiuniversityinfo;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
 /**
  * Created by Md. Ishan Arefin on 19-03-16.
  */
-public class UserUniversityDatabase {
+public class UserUniversityDatabase extends SQLiteOpenHelper{
+    private static final String DBNAME = "appicationdb";
+    private static final int DB_VER = 1;
+
+    public UserUniversityDatabase(Context context)
+    {
+        super(context, DBNAME, null, DB_VER);
+    }
+
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table student ("
+        +"_id integer primary key autoincrement,"
+        +"username text,"
+        +"password text,"
+        +"res_ssc real,"
+        +"res_hsc real,"
+        +"group_ text,"
+        +"type text,"
+        +"hsc_yr integer,"
+        +"blood_grp text);");
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
