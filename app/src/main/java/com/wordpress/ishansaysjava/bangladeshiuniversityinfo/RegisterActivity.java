883c77398e335e09f3c36bd15ab6cef73080eeb6
Md. Ishan Arefin <Md. Ishan Arefin>
2016-04-02 13:41:51 +0600

    RegisterActivity do Fully

--- app/src/main/java/com/wordpress/ishansaysjava/bangladeshiuniversityinfo/RegisterActivity.java
+++ app/src/main/java/com/wordpress/ishansaysjava/bangladeshiuniversityinfo/RegisterActivity.java
@@ -5,15 +5,18 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.text.Editable;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class RegisterActivity extends Activity {
 
@@ -25,48 +28,61 @@ public class RegisterActivity extends Activity {
 
     public void register(View view)
     {
-        ContentValues uservalues = new ContentValues();
-
         String username = ((EditText) findViewById(R.id.username)).getText().toString();
-
-        //determines who is user by saving username even after application kill
-        SharedPreferences sharedPreferences = getSharedPreferences("data", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("current_username", username);
-
-        uservalues.put("username", username);
-        uservalues.put("password", ((EditText) findViewById(R.id.password)).getText().toString());
-        uservalues.put("res_ssc", Double.parseDouble(((EditText) findViewById(R.id.res_ssc)).getText().toString()));
-        uservalues.put("res_hsc", Double.parseDouble(((EditText) findViewById(R.id.res_hsc)).getText().toString()));
-
-        Spinner group = (Spinner) findViewById(R.id.group);
-        if(group.getSelectedItemPosition() == 0)
-        {
-            uservalues.put("group_", "science");
-        }
-        else if(group.getSelectedItemPosition() == 1)
-        {
-            uservalues.put("group_", "commerce");
-        }
-        else uservalues.put("group_", "arts");
-
-        Spinner type = (Spinner) findViewById(R.id.type);
-        if(type.getSelectedItemPosition() == 0)
-        {
-            uservalues.put("type", "regular");
-        }
-        else uservalues.put("type", "irregular");
-
-        uservalues.put("hsc_yr", Integer.parseInt(((EditText) findViewById(R.id.hsc_yr)).getText().toString()));
-        uservalues.put("blood_grp", ((EditText) findViewById(R.id.blood_grp)).getText().toString());
-
         SQLiteOpenHelper useruniversity_dbhelper = new UserUniversityDatabase(this);
         SQLiteDatabase db = useruniversity_dbhelper.getReadableDatabase();
-        db.insert("student", null, uservalues);
+        Cursor cursor = db.query("student", new String[]{"username"}, "username = ?", new String[]{username}, null, null, null);
 
-        Intent intent = new Intent(this, OptionActivity.class);
-        startActivity(intent);
-        finish();
+        if(cursor.moveToFirst())
+        {
+                Toast toast = Toast.makeText(this, "Enter unique username", Toast.LENGTH_LONG);
+                toast.show();
+                Intent intent = new Intent(this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+        }
+
+        else
+        {
+            ContentValues uservalues = new ContentValues();
+            SharedPreferences sharedPreferences = getSharedPreferences("data", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("current_username", username);
+
+            uservalues.put("username", username);
+            uservalues.put("password", ((EditText) findViewById(R.id.password)).getText().toString());
+            uservalues.put("res_ssc", Double.parseDouble(((EditText) findViewById(R.id.res_ssc)).getText().toString()));
+            uservalues.put("res_hsc", Double.parseDouble(((EditText) findViewById(R.id.res_hsc)).getText().toString()));
+
+            Spinner group = (Spinner) findViewById(R.id.group);
+            if(group.getSelectedItemPosition() == 0)
+            {
+                uservalues.put("group_", "science");
+            }
+            else if(group.getSelectedItemPosition() == 1)
+            {
+                uservalues.put("group_", "commerce");
+            }
+            else uservalues.put("group_", "arts");
+
+            Spinner type = (Spinner) findViewById(R.id.type);
+            if(type.getSelectedItemPosition() == 0)
+            {
+                uservalues.put("type", "regular");
+            }
+            else uservalues.put("type", "irregular");
+
+            uservalues.put("hsc_yr", Integer.parseInt(((EditText) findViewById(R.id.hsc_yr)).getText().toString()));
+            uservalues.put("blood_grp", ((EditText) findViewById(R.id.blood_grp)).getText().toString());
+
+
+            db.insert("student", null, uservalues);
+            Intent intent = new Intent(this, OptionActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        cursor.close();
+        db.close();
     }
-
 }
